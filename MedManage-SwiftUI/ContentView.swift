d29389183d56018d606bcded6c8e1bcd5eb75c60
Ritu Raj Singh <rrsingh1101@gmail.com>
2023-04-24 16:28:42 +0530

    Initial Commit

---
+++ MedManage-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MedManage-SwiftUI
+//
+//  Created by Ritu Raj Singh on 24/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedManage-SwiftUI/MedManage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedManage_SwiftUIApp.swift
+//  MedManage-SwiftUI
+//
+//  Created by Ritu Raj Singh on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MedManage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
